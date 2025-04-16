bc14197ec1876d955b24fb42960ffcf9a1d22c95
code-ic <ishchauhan2@gmail.com>
2025-04-16 23:57:17 +0530

    Hopefully Final deploy setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/drive-app-static/', // 👈 Add this line
+  base: '/drive-app-static/', 
   plugins: [react()],
 })
