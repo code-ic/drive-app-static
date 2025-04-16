a391583a6af161b51f9f4e8d6cae1bc90eee0416
admin <admin@admins-MacBook-Air.local>
2025-04-16 23:21:14 +0530

    Updated config for github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/HomePage';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
     </>
   );
 }
